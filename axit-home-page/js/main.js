eba7c580889df3bb33e76c04388aff38e75ee74e
Nikolay Georgiev <47087200+Nikolaj-Georgiev@users.noreply.github.com>
2024-11-16 18:08:35 +0200

    added axit home page to a common repo

---
+++ axit-home-page/js/main.js
@@ -0,0 +1,61 @@
+//////////////////////////////////////////////////////////////
+// Smooth scrolling animation
+const links = document.querySelectorAll('a[href^="#"]');
+const header = document.querySelector('.header');
+const offset = 5 * parseFloat(getComputedStyle(document.documentElement).fontSize); // 5rem
+
+links.forEach(link => {
+  link.addEventListener('click', function (event) {
+    event.preventDefault();
+    const targetId = this.getAttribute('href').substring(1);
+    if (targetId === '') {
+      // scroll to top if href="#"
+      window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+      });
+    } else {
+      const targetElement = document.getElementById(targetId);
+
+      if (targetElement) {
+        window.scrollTo({
+          top: targetElement.offsetTop - offset,
+          behavior: 'smooth'
+        });
+      }
+    }
+  });
+});
+
+////////////////////////////////////////////////////////////
+// Sticky navigation
+const sectionHeroEl = document.querySelector('.hero');
+
+const observer = new IntersectionObserver(function (entries) {
+  const ent = entries[0];
+  if (!ent.isIntersecting) {
+    // console.log(ent);
+    header.classList.add('sticky');
+  }
+
+  if (ent.isIntersecting) {
+    header.classList.remove('sticky');
+  }
+}, {
+  // In the viewport
+  root: null,
+  threshold: 0,
+  rootMargin: '-71px',
+});
+observer.observe(sectionHeroEl);
+
+//////////////////////////
+// Burger menu
+const checkbox = document.querySelector('.menu-toggle');
+const navLinks = document.querySelector('.navbar__links');
+navLinks.addEventListener('click', (e) => {
+  const navLinkClass = e.target.classList[0];
+  if (navLinkClass === 'navbar__link') {
+    checkbox.checked = false;
+  }
+})
